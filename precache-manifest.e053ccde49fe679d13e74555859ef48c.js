dcf5385f7b57d8d08f2d04e954371a8d1966f66c
Sayon Batabyal <sayon.batabyal@kovaion.com>
2022-08-29 01:03:19 +0530

    Updates

---
+++ precache-manifest.e053ccde49fe679d13e74555859ef48c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "338aa2e2f0c66f31ee2b",
+    "url": "/News-Application-Class/static/css/main.40ddfc50.chunk.css"
+  },
+  {
+    "revision": "338aa2e2f0c66f31ee2b",
+    "url": "/News-Application-Class/static/js/main.7333a4d1.chunk.js"
+  },
+  {
+    "revision": "070719c80099be0965a7",
+    "url": "/News-Application-Class/static/js/runtime~main.307531ef.js"
+  },
+  {
+    "revision": "493e9f0f25eb1d1e11fc",
+    "url": "/News-Application-Class/static/js/2.57a0ea94.chunk.js"
+  },
+  {
+    "revision": "bb895545e484ff0164d4",
+    "url": "/News-Application-Class/static/js/3.71a42320.chunk.js"
+  },
+  {
+    "revision": "94cc9fccbddaff2a97aa428e3ffbbb27",
+    "url": "/News-Application-Class/index.html"
+  }
+];
